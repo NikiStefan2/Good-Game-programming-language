53d74a15e2f915f77e6e8695b0b1d89ebdcb1503
nikistefan <nikistefan95@gmail.com>
2021-05-09 10:21:28 +0300

    maked duplication check on variable

--- commands.js
+++ commands.js
@@ -4,12 +4,26 @@ let VariablesNames = [],
 let CreateVariable = (Variablename, VariableValue) => {
     //eval(new ("let " + Variablename + " = " + VariableValue + ";"));
     //console.log(eval(new let("var " + Variablename + " = " + VariableValue + ";", )));
-    VariablesNames.push(Variablename);
-    VariablesValues.push(VariableValue);
+    /**//*VariablesNames.push(Variablename);
+    VariablesValues.push(VariableValue);*/
 
     //console.log(Variables);
     console.log(VariablesNames);
     console.log(VariablesValues);
+
+    if(VariablesValues.length === 0 || VariablesNames.length === 0){
+        VariablesNames.push(Variablename);
+        VariablesValues.push(VariableValue);
+    }
+
+    for(let i = 0; i < VariablesNames; i++){
+        if(VariableName == VariablesNames[i]){
+            VariablesValues[i] = VariableValue;
+        } else{
+            VariablesNames.push(Variablename);
+            VariablesValues.push(VariableValue);
+        }
+    }
 
     //let varsta = 13
 }
@@ -22,16 +36,32 @@ let WriteToConsole = (message) => {
         if (message == VariablesNames[j]) {
             //eval('console.log(' + message + ');');
 
+            Textarea.value += "\n" + VariablesValues[j];
 
             console.log(VariablesNames[j]);
             console.log(VariablesValues[j]);
 
             j = 0;
+
+            break;
         } else {
-            eval('console.log("' + message + '");');
+            //eval('console.log("' + message + '");');
             // eval('console.log("' + message + '");');
             console.log(VariablesNames);
+
+            Textarea.value += "\n" +  message;
+
+            break;
         }
         //console.log(message);
     }
+}
+
+let ModifyVariable = (index, newValue) => {
+    /*for(let i = 0; i < VariablesNames; i++){
+        if(NumeVariabila != VariablesNames){
+            CreateVariable()
+        }
+    }*/
+    VariablesValues[index] = newValue;
 }
