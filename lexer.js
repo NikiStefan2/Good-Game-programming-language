143f83d6c9c327211b1ad3935abf1fda2def7573
nikistefan <nikistefan95@gmail.com>
2021-05-11 22:45:07 +0300

    maked tokenize method

--- lexer.js
+++ lexer.js
@@ -1,10 +1,75 @@
-class Token{
-    constructor(word, Token_Type, Token_Value){
-        this.word = word;
+let operators = ["=", "-", "+", "*", "/"];
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+class Token {
+    constructor(source, Token_Type, Token_Value) {
+        this.source = source;
         this.Token_Type = Token_Type;
         this.Token_Value = Token_Value;
-    } 
-    Tokenize(){
-        
+    }
+    Tokenize() {
+
+        let tokens = [];
+
+        let Index = 0;
+
+        let words = this.source.split(' ');
+
+        while (Index <= words.length) {
+            if (words[Index] == "ver") {//var vaer var
+                this.Token_Type = "VARIABLE";
+                this.Token_Value = words[Index];
+                tokens.push([this.Token_Type, this.Token_Value]);
+            }
+            Index++;
+        }
+        operators.forEach(operator => {
+            if (words[Index] == operator) {
+
+                this.Token_Type = "OPERATOR";
+                // this.Token_Value = words[Index];
+                tokens.push(this.Token_Type);
+
+                // console.log(words[Index]);
+
+                /*if (words[Index] == operators[0]) {
+                    this.Token_Type = "VARIABLE";
+                    this.Token_Value = words[Index + 1];
+                    tokens.push([this.Token_Type, this.Token_Value]);
+                }
+                if (words[Index] == operators[1]) {
+                    this.Token_Type = "VARIABLE";
+                    this.Token_Value = words[Index + 1];
+                    tokens.push([this.Token_Type, this.Token_Value]);
+                }
+                if (words[Index] == operators[2]) {
+                    this.Token_Type = "VARIABLE";
+                    this.Token_Value = words[Index + 1];
+                    tokens.push([this.Token_Type, this.Token_Value]);
+                }
+                if (words[Index] == operators[3]) {
+                    this.Token_Type = "VARIABLE";
+                    this.Token_Value = words[Index + 1];
+                    tokens.push([this.Token_Type, this.Token_Value]);
+                }
+                if (words[Index] == operators[4]) {
+                    this.Token_Type = "VARIABLE";
+                    this.Token_Value = words[Index + 1];
+                    tokens.push([this.Token_Type, this.Token_Value]);
+                }*/
+            }
+        });
+        numbers.forEach(number => {
+            if (words[Index].includes(number)) {
+
+                this.Token_Type = "INTEGER";//TRUBUIA STRING DAR FACUO PRIMU INTEGER ;) STIAM EU BRESIT
+                this.Token_Value = words[Index];
+                tokens.push([this.Token_Type, this.Token_Value]);
+            }
+
+        });
+
+
+        return tokens;
     }
 }
---
+++ main.js
@@ -0,0 +1,12 @@
+document.getElementById("Compile-Btn-Submit").addEventListener('click', function () {
+
+    let Tocen = new Token(Textarea_Submit.value);
+
+    console.log(Tocen.Tokenize());
+
+
+    console.log("haha");
+
+})
+
+console.log("fdfs");
